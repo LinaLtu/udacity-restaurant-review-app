cbd8f6703cf75f1e661c732b5224a579d9bbc253
LinaLtu <lsuodyte@babbel.com>
2018-07-14 12:03:46 +0200

    Added Service Worker

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener("fetch", function(event) {
+  console.log("Handling fetch event for", event.request.url);
+
+  event.respondWith(
+    caches.open("response-cache").then(function(cache) {
+      return cache
+        .match(event.request)
+        .then(function(response) {
+          if (response) {
+            console.log("Found response in cache:", response);
+
+            return response;
+          }
+
+          console.log("Fetching request from the network");
+
+          return fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+
+            return networkResponse;
+          });
+        })
+        .catch(function(error) {
+          console.error("Error in fetch handler:", error);
+
+          throw error;
+        });
+    })
+  );
+});
