9f7d9d5d6b075b7ec2103d87d930717bf17dac75
LinaLtu <lsuodyte@babbel.com>
2018-07-15 16:31:05 +0200

    Generated Alt Attributed for Better Accesability:

--- js/main.js
+++ js/main.js
@@ -169,6 +169,7 @@ createRestaurantHTML = restaurant => {
 
   const image = document.createElement("img");
   image.className = "restaurant-img";
+  image.alt = restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
